9cf11d0e9e30a92521b1eb06dbc43e948fe0ebfb
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2023-07-15 10:32:33 -0700

    player controller script

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MovementController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed;
+
+    Vector2 _movement;
+
+    Rigidbody2D _rb;
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _movement * _speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputValue value) {
+        _movement = value.Get<Vector2>();
+    }
+}
